b1441a36ab48292513812f52b47fcac4e319e857
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-04-12 16:39:31 +0900

    Renamed and implemented a comic book character controller

---
+++ APIMarvel/Sources/ViewController/CharacterViewVontroller.swift
@@ -0,0 +1,85 @@
+import UIKit
+import Alamofire
+
+final class CharacterViewVontroller: UIViewController {
+
+    // MARK: - Properties
+
+    private var character: Character?
+    var resourceURI: String?
+
+    // MARK: - Outlets
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(SeriesTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        fetchCharacter()
+    }
+
+    // MARK: - Networking
+
+    private func fetchCharacter() {
+        guard let resourceURI else { return }
+        let urlReplacing = resourceURI.replacingOccurrences(of: "http", with: "https")
+        let url = "\(urlReplacing)?ts=\(APIConfig.ts)&apikey=\(APIConfig.publicKey)&hash=\(APIConfig.hash)"
+        let request = AF.request(url)
+        request.responseDecodable(of: Characters.self) { (data) in
+            guard let char = data.value else { return }
+            self.character = char.data?.results?.first
+            self.tableView.reloadData()
+        }
+    }
+
+    // MARK: - Configure
+
+    private func configureView() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - DataSource
+
+extension CharacterViewVontroller: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  character?.series?.items?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? SeriesTableViewCell else {
+            return UITableViewCell()
+        }
+        if let series = character?.series {
+            cell.series = series.items?[indexPath.row]
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
+    }
+}
+
+// MARK: - Delegate
+
+extension CharacterViewVontroller: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
---
+++ APIMarvel/Sources/ViewController/ComicsViewController.swift
@@ -0,0 +1,89 @@
+import UIKit
+import Alamofire
+
+final class ComicsViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private var characters: Characters?
+
+    // MARK: - Outlets
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CharactersTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        fetchComics()
+    }
+
+    // MARK: - Networking
+
+    private func fetchComics() {
+        let url = "https://gateway.marvel.com/v1/public/comics/1332/characters?ts=\(APIConfig.ts)&apikey=\(APIConfig.publicKey)&hash=\(APIConfig.hash)"
+        let request = AF.request(url)
+        request.responseDecodable(of: Characters.self) { (data) in
+            guard let characters = data.value else { return }
+            self.characters = characters
+            self.title = characters.attributionText
+            self.tableView.reloadData()
+        }
+    }
+
+    // MARK: - Configure
+
+    private func configureView() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - DataSource
+
+extension ComicsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters?.data?.results?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CharactersTableViewCell else {
+            return UITableViewCell()
+        }
+        if let character = self.characters?.data?.results?[indexPath.row] {
+            cell.character = character
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        70
+    }
+}
+
+// MARK: - Delegate
+
+extension ComicsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let charController = CharacterViewVontroller()
+        if let char = characters?.data?.results?[indexPath.row] {
+            charController.title = "Character series \(char.name)"
+            charController.resourceURI = char.resourceURI
+        }
+        navigationController?.pushViewController(charController, animated: true)
+    }
+}
